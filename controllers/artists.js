0b3354eafcd6ab8c0cc5bbadb2384a3eef55935f
Jaytee <Padilla.Jaytee@gmail.com>
2020-11-05 19:55:09 -0800

    Added CRUD functionality to /artist related routes

---
+++ controllers/artists.js
@@ -0,0 +1,101 @@
+const { Artist, Song } = require('../models');
+
+// *** CREATE ***
+const createArtist = async (req, res) => {
+  try {
+    const artist = await Artist.create(req.body);
+    return res.status(201).json({
+      artist
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+}
+
+// *** READ ***
+const getAllArtists = async (req, res) => {
+  try {
+    const artists = await Artist.findAll({
+      include: [
+        {
+          model: Song
+        }
+      ]
+    });
+
+    return res.status(200).json({artists});
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+}
+
+const getArtistById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const artist = await Artist.findOne({
+      where: { id: id },
+      include: [
+        {
+        model: Song
+        }
+      ]
+    });
+
+    if (artist) {
+      return res.status(200).json({artist});
+    }
+
+    return res.status(404).send('Artist with the specified ID does not exist');
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+}
+
+// *** UPDATE ***
+const updateArtist = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const [updated] = await Artist.update(req.body, {
+      where: {id: id}
+    });
+
+    if (updated) {
+      const updatedArtist = await Artist.findOne({where: {id: id}});
+      
+      return res.status(200).json({Artist: updatedArtist});
+    }
+
+    throw new Error('Artist not found');
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+// *** DELETE ***
+const deleteArtist = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deleted = await Artist.destroy({
+      where: {id:id}
+    });
+
+    if (deleted) {
+      return res.status(204).send('Artist deleted');
+    }
+
+    throw new Error('Artist not found');
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+module.exports = {
+  createArtist,
+  getAllArtists,
+  getArtistById,
+  updateArtist,
+  deleteArtist
+}
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 const { Router } = require('express');
-// const controllers = require('../controllers');
+const artists = require('../controllers/artists');
 
 const router = Router();
 
 router.get('/', (req, res) => {
   res.send('Server is up and running');
 });
+
+// ARTIST
+router.get('/artists', artists.getAllArtists);
+router.get('/artists/:id', artists.getArtistById);
+router.patch('/artists/:id', artists.updateArtist);
+router.delete('/artists/:id', artists.deleteArtist);
+router.post('/artists', artists.createArtist);
 
 module.exports = router;
